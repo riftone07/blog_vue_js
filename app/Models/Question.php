b881d5a0ca892a3ef4a61fad993365bf34511d22
unknown <riftone07@gmail.com>
2019-04-19 23:58:42 +0200

    relationship

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,7 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\User;
+
 class Question extends Model
 {
-    //
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
+
+	
+    public function category()
+    {
+    	return $this->belongsTo(Category::class);
+    }
+
+	
+    public function replies()
+    {
+    	return $this->hasMany(Reply::class);
+    }
+
+
 }
--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Reply extends Model
 {
-    //
+    	
+    public function question()
+    {
+    	return $this->belongsTo(Question::class);
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
+ 
+    public function likes()
+    {
+    	return $this->hasMany(Like::class);
+    }
+
 }
